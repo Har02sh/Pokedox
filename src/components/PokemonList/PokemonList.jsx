32dd9778da901a83cc7867c45a53842583f0cf5a
Har02sh <harsh.vardhan.020102@gmail.com>
2025-01-30 00:56:54 +0530

    Pagination Added

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
 import "./PokemonList.css";
 import Pokemon from "../Pokemon/Pokemon";
+import Loader from "../Loader/Loader"
 
 function PokemonList(){
 
     const [isLoading, setIsLoading] = useState(true);
     const [pokemonList, setPokemonList] = useState([]);
+    const [pokedexUrl,setPokeDexUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
+    const [prevUrl,setPrevUrl] = useState('');
+    const [nextUrl,setNextUrl] = useState('');
 
     async function getPokemons(){
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+        const response = await fetch(pokedexUrl);
         const data = await response.json();
         const pokemonUrls = data.results.map(pokemon => fetch(pokemon.url));
-        
+        setPrevUrl(data.previous);
+        setNextUrl(data.next);
+
         const urlResult = await Promise.all(pokemonUrls);
         const pokemonDataResponse = await Promise.all(urlResult.map(urlResponse => urlResponse.json()));
         
@@ -29,19 +35,19 @@ function PokemonList(){
 
     useEffect(()=>{
         getPokemons();
-    }, [])
+    }, [pokedexUrl])
 
     return(
         <>
             <h4>Pokemon List</h4>
             <div className="pokemon-wrapper-div">
-            { isLoading ? "Data Downloading..." : 
+            { isLoading ? <Loader/> : 
               pokemonList.map(poke => <Pokemon name={poke.name} image={poke.image}  key={poke.id} />)
             }
             </div>
             <div className="buttons">
-                <button id="prev">Prev</button>
-                <button id="next">Next</button>
+                <button disabled={!prevUrl} onClick={()=> {setPokeDexUrl(prevUrl); setIsLoading(true)}}>Prev</button>
+                <button disabled={!nextUrl} onClick={() => {setPokeDexUrl(nextUrl); setIsLoading(true)}}>Next</button>
             </div>
         </>
         
